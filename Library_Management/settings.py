bdb47aa0e39c3be8519c10b06518d64cf8e87afb
Tasmia-Chowdhury-Alif <tasmiachowdhuryalif222@gmail.com>
2025-06-16 19:48:57 +0600

    upgaded settings.py for deployment

--- Library_Management/settings.py
+++ Library_Management/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = ['https://bookpilot-library-management.onrender.com']
 
 
 # Application definition
